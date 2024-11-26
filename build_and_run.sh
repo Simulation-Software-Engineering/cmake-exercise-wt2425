668daad1c6f87c078b36b8d802b7e9782e9a9e37
Pinku-Neko <henryiakvictor@gmail.com>
2024-11-26 13:30:48 +0100

    set up docker, shell removes build before start, implement cmakelist for make, uncomment functions in main.cpp

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,12 @@
+# Normalize line endings to LF
+sed -i 's/\r$//' "$0"
+
 #!/usr/bin/env bash
+
+# Clean up the build directory if it exists
+if [ -d "build" ]; then
+    rm -rf build
+fi
 
 mkdir build
 cd build
